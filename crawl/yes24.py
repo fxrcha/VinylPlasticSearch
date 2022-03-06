cdab5852362fd296d8e70b1fd4c37f942e40a0e6
fxrcha <truetype24@gmail.com>
2022-03-06 11:16:27 +0900

    shitpost

--- crawl/yes24.py
+++ crawl/yes24.py
@@ -28,6 +28,22 @@ class YES24(Requester):
 
         return res
 
+    def __grab_artist(self, asset) -> str:
+        if asset.find("span", {"class": "authPub info_auth"}).find(
+            "span", {"class": "moreAuthArea"}
+        ):
+            asset.find("span", {"class": "authPub info_auth"}).find(
+                "span", {"class": "moreAuthArea"}
+            ).decompose()
+
+        return asset.find("span", {"class": "authPub info_auth"}).text
+
+    def __get_status(self, asset) -> str:
+        if asset.find("span", {"class": "btn_row soldOut"}):
+            return "품절"
+
+        return "구매 가능"
+
     async def crawl(self, album, artist) -> List[SearchResult]:
         self.soup = await self.search_album(album, artist)
 
@@ -39,14 +55,17 @@ class YES24(Requester):
             item_info = i.find("div", {"class": "item_info"})
             btns = i.find("div", {"class": "item_btnCol"})
 
+            if item_info.find("span", {"class": "txt_sale"}):
+                sale_rate = item_info.find("span", {"class": "txt_sale"}).text
+
             result.append(
                 SearchResult(
                     typ=item_info.find("span", {"class": "gd_res"}).text,
                     album_title=self.__grab_title(asset=item_info),
-                    artist=item_info.find("span", {"class": "authPub info_auth"}).text,
+                    artist=self.__grab_artist(asset=item_info),
                     price=item_info.find("strong", {"class": "txt_num"}).text,
-                    status="",
-                    sale_rate="",
+                    status=self.__get_status(asset=btns),
+                    sale_rate=sale_rate,
                 )
             )
 
--- scraps/asdf.py
+++ scraps/asdf.py
@@ -7,9 +7,42 @@ def asdf() -> BeautifulSoup:
     return BeautifulSoup(a, "html.parser")
 
 
+def shitpost(asset) -> str:
+    asdf = asset.find("span", {"class": "authPub info_auth"})
+    if asdf.find("span", {"class": "moreAuthArea"}):
+        asdf.find("span", {"class": "moreAuthArea"}).decompose()
+
+    asdf = asset.find("span", {"class": "authPub info_auth"}).text
+    return asdf
+
+
+def second_shitpost(asset) -> str:
+    if asset.find("span", {"class": "btn_row soldOut"}):
+        return "품절"
+
+    return "구매 가능"
+
+
 def test(s: BeautifulSoup):
     for i in s.find("ul", {"id": "yesSchList"}).find_all("div", {"class": "itemUnit"}):
+        print("start")
         item_info = i.find("div", {"class": "item_info"})
+        btns = i.find("div", {"class": "item_btnCol"})
+
+        print(item_info.find("span", {"class": "gd_res"}).text)
+        print("b")
+        print(item_info.find("a", {"class": "gd_name"}).text)
+        print("asdf")
+        print(shitpost(item_info).strip())
+        print("asdfasdf")
+        print(item_info.find("strong", {"class": "txt_num"}).text)
+        print("asdfasdfasdfasdf")
+        if item_info.find("span", {"class": "txt_sale"}):
+            print(item_info.find("span", {"class": "txt_sale"}).text)
+
+        print("asfdasdgtj")
+        print(second_shitpost(btns))
+        print("end")
 
 
 if __name__ == "__main__":
